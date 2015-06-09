976ae693373bff050e4f9e148003dee542aacbcb
Anantha Kumaran <ananthakumaran@gmail.com>
2015-06-09 18:13:25 +0530

    add url

--- typescript-mode.el
+++ typescript-mode.el
@@ -20,6 +20,7 @@
 ;;     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ;; -------------------------------------------------------------------------------------------
 
+;; URL: http://github.com/ananthakumaran/typescript.el
 ;; Version: 0.1
 ;; Keywords: typescript languages
 ;; Package-Requires: ()
