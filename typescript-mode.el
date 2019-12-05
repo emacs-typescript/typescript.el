c766ec8009d5de0fcf605eac6238ea6d6c0f3d6e
Sebastian Sturm <mail@sebastian-sturm.de>
2019-12-05 23:35:06 +0100

    Include symbol constituents in typescript--function-call-re

--- typescript-mode.el
+++ typescript-mode.el
@@ -297,7 +297,7 @@ Match group 1 is MUMBLE.")
    '("console"))
   "Regular expression matching builtins.")
 
-(defconst typescript--function-call-re "\\(\\w+\\)\\(<.+>\\)?\s*("
+(defconst typescript--function-call-re "\\(\\(?:\\w\\|\\s_\\)+\\)\\(<.+>\\)?\s*("
   "Regular expression matching function calls.")
 
 (defconst typescript--font-lock-keywords-1
