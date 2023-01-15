7675bab5f2427f16424a8b531cfb10ffe29c2ef3
Jon Dufresne <jon.dufresne@gmail.com>
2023-01-15 15:14:49 -0800

    Add .tsx to auto-mode-alist
    Add out-of-the-box support for using typescript-mode on *.tsx files.
    Avoids repetitive manual configuration for end users.

--- typescript-mode.el
+++ typescript-mode.el
@@ -3119,7 +3119,7 @@ Key bindings:
      (folding-add-to-marks-list 'typescript-mode "// {{{" "// }}}" )))
 
 ;;;###autoload
-(add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
+(add-to-list 'auto-mode-alist '("\\.tsx?\\'" . typescript-mode))
 
 (provide 'typescript-mode)
 
